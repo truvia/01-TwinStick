a3eebc3b0e0a806cf48b842d02963f7760703f59
DESKTOP-KN4T54I\Charlie <charlie.trew@gmail.com>
2017-01-14 16:02:02 +0000

    Addes script

---
+++ Assets/MyScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyScript : MonoBehaviour {
+
+	private bool done;
+	private int three;
+	// Use this for initialization
+	void Start () {
+
+		print ("hello");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+}
